57ee9d702fa5f086270364fffef6afa084f6656f
bulnik_paw <bulnikn@gmail.com>
2021-02-20 15:44:12 +0000

    Add  Medical purposes

--- medicalcard/medcard/admin.py
+++ medicalcard/medcard/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Patient, Main, Doctor, Clinical, MedicalProcedures
+from .models import Patient, MedicalHistory, Doctor, Clinical, MedicalProcedures
+from .models import Operation, Examination, Vaccination, MedicalPurpose, Analysis
 
 admin.site.register(Patient)
-admin.site.register(Main)
 admin.site.register(Doctor)
 admin.site.register(Clinical)
+
 admin.site.register(MedicalProcedures)
+admin.site.register(MedicalHistory)
+
+admin.site.register(Examination)
+admin.site.register(Vaccination)
+admin.site.register(Analysis)
+admin.site.register(MedicalPurpose)
+admin.site.register(Operation)
+
+
+
--- medicalcard/medcard/models.py
+++ medicalcard/medcard/models.py
@@ -1,10 +1,5 @@
 from django.db import models
 
-class Clinical(models.Model):
-    name = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.name
 
 
 class User(models.Model):
@@ -19,32 +14,82 @@ class User(models.Model):
     class Meta:
         abstract = True
 
-class Doctor(User):
-    #city = models.CharField(max_length=20, default="No city")
-    clinical = models.ForeignKey(Clinical, on_delete = models.DO_NOTHING,default=1)
-    #models.DO_NOTHING not logic, but SET_NULL, except error
-    def __str__(self):
-        return f"Dr. {self.surname} {self.name}"
 
-class Patient(User):
-    #city = models.CharField(max_length=20)
-    job = models.CharField(max_length=20)
-    medical_insurance = models.BooleanField()
-    my_doctor = models.ForeignKey(Doctor, on_delete = models.DO_NOTHING,default=1)
-    def __str__(self):
-        return f"{self.surname} {self.name}"
-
-
-class Main(models.Model):
-    date = models.DateField()
-    #patient = models.ForeignKey(Patient, on_delete = models.DO_NOTHING)
-
-
-    def __str__(self):
-        return f"{self.id} {self.date} {self.patient.__str__()}"
-
-class MedicalProcedures(models.Model):
+class Clinical(models.Model):
     name = models.CharField(max_length=20)
 
     def __str__(self):
         return self.name
+
+
+class Doctor(User):
+    #city = models.CharField(max_length=20, default="No city")
+    clinical = models.ForeignKey(Clinical, on_delete = models.DO_NOTHING, null=True)
+    #models.DO_NOTHING not logic, but SET_NULL, except error
+    def __str__(self):
+        return f"Dr. {self.surname} {self.name}"
+
+class MedicalProcedures(models.Model):
+    name = models.CharField(max_length=20)
+
+    in_clinik = models.ForeignKey(Clinical, on_delete = models.DO_NOTHING,default=1)
+
+    def __str__(self):
+        return f"{self.name} {self.in_clinik}"
+
+class MedicalHistory(models.Model):
+    date = models.DateField()
+    medical_procedures = models.ForeignKey(MedicalProcedures, on_delete = models.DO_NOTHING, null=True)
+
+
+    def __str__(self):
+        return f" {self.date}{self.medical_procedures} "
+
+
+class Patient(User):
+    city = models.CharField(max_length=20, null=True)
+    job = models.CharField(max_length=20)
+    medical_insurance = models.BooleanField()
+    my_doctor = models.ForeignKey(Doctor, on_delete = models.DO_NOTHING, null=True)
+    #my_med_history =models.ForeignKey(MedicalHistory, on_delete = models.DO_NOTHING,default=1)
+
+    def __str__(self):
+        return f"{self.surname} {self.name}"
+
+class MedProc(models.Model):
+    date = models.DateField()
+    patient = models.ForeignKey(Patient, on_delete = models.DO_NOTHING, null=True)
+    clinical = models.ForeignKey(Clinical, on_delete = models.DO_NOTHING, null=True)
+
+    def __str__(self):
+        return f" {self.date}{self.patient} "
+
+    class Meta:
+        abstract = True
+
+class Examination(MedProc):
+    time=models.TimeField(auto_now=True)
+    visit_purpose = models.CharField(max_length=256, null=True)
+    doctor = models.ForeignKey(Doctor, on_delete = models.DO_NOTHING, null=True)
+    patient_status  = models.CharField(max_length=256, null=True)
+
+class Vaccination(MedProc):
+    vaccine = models.CharField(max_length=256,  null=True)
+    info = models.CharField(max_length=256,  null=True)
+
+class Analysis(MedProc):
+    name_analysis = models.CharField(max_length=256,  null=True)
+    result_analysis = models.CharField(max_length=256,  null=True)
+
+
+class MedicalPurpose(MedProc):
+    direction = models.CharField(max_length=256, null=True)
+
+class Operation(MedProc):
+    name = models.CharField(max_length=256, null=True)
+    doctor = models.ForeignKey(Doctor, on_delete = models.DO_NOTHING, null=True)
+    info = models.CharField(max_length=256,  null=True)
+
+
+
+
