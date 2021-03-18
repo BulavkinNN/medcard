197f5f5c70ccc51cccf822829b337587b9fe7bf0
nikolay <bulnikn@gmail.com>
2021-03-18 19:30:26 +0200

    role

--- medicalcard/authentication/views.py
+++ medicalcard/authentication/views.py
@@ -4,9 +4,11 @@ from django.http import HttpResponseRedirect
 from .forms import AuthenticationForm
 from medcard.models import UserAccount
 from django.urls import reverse
+from django.contrib.auth.hashers import check_password
+from django.conf import settings
+
 
 def index(request):
-
     list_role = Role.objects.all()
     if request.method == "POST":
         context = '''
@@ -22,10 +24,16 @@ def index(request):
                                    '''
             mob_tel = form.cleaned_data['mob_tel']
             password = form.cleaned_data['password']
-            role = request.POST['role']
-            # []with role need get from Role model, not manual!!
-            if role in ['pacient','doctor']:
-                return HttpResponseRedirect(reverse(role))
+            try:
+                user = UserAccount.objects.get(mob_tel=mob_tel)
+            except Exception as e:
+                print (e)
+                return HttpResponseRedirect(reverse(index))
+            if user.check_password(password):
+                role = request.POST['role']
+                # []with role need get from Role model, not manual!!
+                if role in ['patient', 'doctor']:
+                    return HttpResponseRedirect(reverse(role))
     else:
         form = AuthenticationForm()
         context = '''
@@ -34,11 +42,8 @@ def index(request):
                    других профилей в админ панели.</p>
                    '''
 
-
-
-    return render(request,'authentication/index.html',
-                    {"context":context,
-                     "list_role":list_role,
-                     'form': form
-                    })
-
+    return render(request, 'authentication/index.html',
+                  {"context": context,
+                   "list_role": list_role,
+                   'form': form
+                   })
