71e00943cb780c515105da8b5fdcde8ab4132481
Konark-Web <konark666@gmail.com>
2021-05-24 01:05:54 +0300

    Added styles Added header Added styles for footer Added styles for login form Fixed login form for authenticated users (added redirect)

--- medicalcard/authentication/views.py
+++ medicalcard/authentication/views.py
@@ -8,7 +8,7 @@ from authentication.forms import AuthenticationCustomForm
 class LoginCustomView(LoginView):
     template_name = 'authentication/login.html'
     form_class = AuthenticationCustomForm
-    redirect_authenticated_user = False
+    redirect_authenticated_user = True
 
 
 class LogoutCustomView(LogoutView):
